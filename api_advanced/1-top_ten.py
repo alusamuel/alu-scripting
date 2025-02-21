7d53781beb5d7e78f74fe7262c979117f974efe7
alusamuel <s.rurangami@alustudent.com>
2025-02-21 22:10:02 +0200

    API advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3
-"""Reddit API Query - Top 10 Hot Posts"""
+"""DOCS"""
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts from a subreddit"""
-    reddit_url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'MyRedditBot/0.1 by YourUsername'}
-
-    response = requests.get(reddit_url, headers=headers, allow_redirects=False)
-
-    # Check if request is successful
-    if response.status_code == 200:
-        try:
-            data = response.json().get('data', {})
-            posts = data.get('children', [])
-
-            if posts:
-                for post in posts[:10]:
-                    print(post['data']['title'])
-            else:
-                print(None)
-        except (ValueError, KeyError):
-            print(None)
-
-    else:
+    """Docs"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'MyBot/0.0.1'}
+    params = {'limit': 10}
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    if response.status_code != 200:
         print(None)
+        return
+    data = response.json().get('data', {})
+    posts = data.get('children', [])
+    for post in posts:
+        print(post.get('data', {}).get('title'))
