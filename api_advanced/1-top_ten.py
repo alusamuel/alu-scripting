ec2ea42ef9f9b08f17a9b321bfebe48c908d9b8d
alusamuel <s.rurangami@alustudent.com>
2025-02-21 22:13:20 +0200

    API advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,14 +5,14 @@ import requests
 
 def top_ten(subreddit):
     """Docs"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'MyBot/0.0.1'}
-    params = {'limit': 10}
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    if response.status_code != 200:
+    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    headers = headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()['data']
+        for post in data['children'][:10]:
+            print(post['data']['title'])
+    else:
         print(None)
-        return
-    data = response.json().get('data', {})
-    posts = data.get('children', [])
-    for post in posts:
-        print(post.get('data', {}).get('title'))
