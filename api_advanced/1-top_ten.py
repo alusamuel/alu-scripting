a43ca5f44b581969bb96a3b07a5ee098ef4826b8
alusamuel <s.rurangami@alustudent.com>
2025-02-21 21:36:37 +0200

    API advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-"""DOC"""
+"""DOCS"""
 import requests
 
+
 def top_ten(subreddit):
-    """DOC"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'limit': 10}
-    
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    
-    if response.status_code != 200:
-        print(None)
-        return
-    
-    try:
-        data = response.json()
-        posts = data['data']['children']
-        
-        for post in posts:
+    """Docs"""
+    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    headers = headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()['data']
+        for post in data['children'][:10]:
             print(post['data']['title'])
-    except (KeyError, ValueError):
+    else:
         print(None)
